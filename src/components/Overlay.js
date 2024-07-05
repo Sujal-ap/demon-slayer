4d6c5b5e2b28da0489216b34dcd08fc66da0b66f
Sujal-ap <ushabalmiki8@gmail.com>
2024-07-05 13:43:33 +0530

    shreya will definetely fall in love with me soon i think she is already falls in love with me

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../styles/overlay.css';
 
 const Overlay = ({ character, onClose }) => {
+  useEffect(() => {
+    const blueBg = document.getElementById('blueBg');
+    setTimeout(() => {
+      blueBg.classList.add('active');
+    }, 100); // Adjust delay as needed
+  }, []);
+
   return (
     <div className="overlay">
+      <div id="blueBg" className="blueBg"></div>
       <div className={`overlay-content ${character.className}-overlay`}>
         <button className="close-button" onClick={onClose}>X</button>
         <div className="overlay-text">
